eb1453c0851bf1cc5b05542b74f0b8c6595237ef
TambyGit <hei.tamby@gmail.com>
2026-03-18 17:19:18 +0300

    add: setup for TD2 et TD3

---
+++ src/main/java/com/spring/tdspring_2_et_3/Application.java
@@ -0,0 +1,13 @@
+package com.spring.tdspring_2_et_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/tdspring_2_et_3/TdSpring2Et3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.tdspring_2_et_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdSpring2Et3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
