74b95e43b1a5bb9240b8afc062698baba9ac241f
TambyGit <hei.tamby@gmail.com>
2026-03-18 18:46:34 +0300

    add: Get students with Headers JSON/plai types

--- src/main/java/com/spring/tdspring_2_et_3/controller/StudentController.java
+++ src/main/java/com/spring/tdspring_2_et_3/controller/StudentController.java
@@ -1,23 +1,40 @@
 package com.spring.tdspring_2_et_3.controller;
 
 import com.spring.tdspring_2_et_3.Student;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class StudentController {
 
     private static final List<Student> studentList = new ArrayList<Student>();
 
-    @PostMapping("/student")
+    @PostMapping("/students")
     public List<String> addStudents (@RequestBody List<Student> student) {
         studentList.addAll(student);
         return studentList.stream()
                 .map(s -> s.getFirstName() + " " + s.getLastName())
                 .toList();
+    }
+
+    @GetMapping("/students")
+    public ResponseEntity<?> getStudents(@RequestHeader("Accept") String accetHeader) {
+        if ("text/plain".equals(accetHeader)) {
+            String name = studentList.stream().map(s -> s.getFirstName() + " " + s.getLastName())
+                    .collect(Collectors.joining(", "));
+            return ResponseEntity.ok()
+                    .body(name);
+        }
+        else if ("application/json".equals(accetHeader)) {
+            return ResponseEntity.ok()
+                    .body(studentList);
+        }else {
+            return ResponseEntity.badRequest()
+                    .body("Format non supporté");
+        }
     }
 }
