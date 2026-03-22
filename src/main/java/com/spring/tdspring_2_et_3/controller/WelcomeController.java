4f32965061a2ec31aa9231fa71af5638ed11e53d
TambyGit <hei.tamby@gmail.com>
2026-03-22 19:08:29 +0300

    update: refractor Welcome into a HTTP response

--- src/main/java/com/spring/tdspring_2_et_3/controller/WelcomeController.java
+++ src/main/java/com/spring/tdspring_2_et_3/controller/WelcomeController.java
@@ -1,13 +1,22 @@
 package com.spring.tdspring_2_et_3.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
     @GetMapping("/welcome")
-    public String welcome(String name) {
-        return "Welcome " + name;
+    public ResponseEntity<String> welcome(@RequestParam String name) {
+        if (name == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("Parameter name is empty or null");
+        }else {
+            return ResponseEntity.ok().
+                    body("Welcome " + name);
+        }
     }
 }
