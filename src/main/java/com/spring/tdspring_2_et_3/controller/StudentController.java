bc84a49557d16e785dd70e9f94aea67bf676d4a3
TambyGit <hei.tamby@gmail.com>
2026-03-24 20:59:06 +0300

    update:StudentController with less charge

--- src/main/java/com/spring/tdspring_2_et_3/controller/StudentController.java
+++ src/main/java/com/spring/tdspring_2_et_3/controller/StudentController.java
@@ -1,6 +1,9 @@
 package com.spring.tdspring_2_et_3.controller;
 
 import com.spring.tdspring_2_et_3.Student;
+import com.spring.tdspring_2_et_3.exeption.BadRequestExeption;
+import com.spring.tdspring_2_et_3.service.StudentService;
+import com.spring.tdspring_2_et_3.validator.StudentValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,50 +15,51 @@ import java.util.stream.Collectors;
 @RestController
 public class StudentController {
 
-    private static final List<Student> studentList = new ArrayList<Student>();
+    private final StudentService studentService =  new StudentService();
+    private final StudentValidator studentValidator = new StudentValidator();
 
     @PostMapping("/students")
-    public ResponseEntity <List<Student>> createStudent(@RequestBody List<Student> student) {
+    public ResponseEntity <?> createStudent(@RequestBody List<Student> students) {
         try{
-            studentList.addAll(student);
+           studentValidator.validate(students);
+           List<Student> saved = studentService.saveStudentOnMemory(students);
+           return ResponseEntity
+                   .status(HttpStatus.CREATED)
+                   .body(saved);
+        }catch (BadRequestExeption e){
             return ResponseEntity
-                    .status(HttpStatus.CREATED)
-                    .body(studentList);
-
-        }catch (Exception e){
-            return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(studentList);
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(e.getMessage());
         }
     }
 
-    @GetMapping("/students")
-    public ResponseEntity<?> getStudents(@RequestHeader(value = "Accept", required = false) String accetHeader) {
-        try{
-            if (accetHeader == null || accetHeader.isEmpty()) {
-                return ResponseEntity
-                        .status(HttpStatus.BAD_REQUEST)
-                        .body("Parameter accetHeader is empty or null");
-            }
-            if ("text/plain".equals(accetHeader)) {
-                String names = studentList.stream()
-                        .map(s -> s.getFirstName() + " " + s.getLastName())
-                        .collect(Collectors.joining(", "));
-                return ResponseEntity.ok()
-                        .body(names);
-            }else if  ("application/json".equals(accetHeader)) {
-                return ResponseEntity.ok()
-                        .body(studentList);
-            }else{
-                return  ResponseEntity
-                        .status(HttpStatus.NOT_IMPLEMENTED)
-                        .body("Parameter accetHeader is not supported");
-            }
-
-        }catch (Exception e){
-            return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Error Server");
-        }
-    }
+//    @GetMapping("/students")
+//    public ResponseEntity<?> getStudents(@RequestHeader(value = "Accept", required = false) String accetHeader) {
+//        try{
+//            if (accetHeader == null || accetHeader.isEmpty()) {
+//                return ResponseEntity
+//                        .status(HttpStatus.BAD_REQUEST)
+//                        .body("Parameter accetHeader is empty or null");
+//            }
+//            if ("text/plain".equals(accetHeader)) {
+//                String names = studentList.stream()
+//                        .map(s -> s.getFirstName() + " " + s.getLastName())
+//                        .collect(Collectors.joining(", "));
+//                return ResponseEntity.ok()
+//                        .body(names);
+//            }else if  ("application/json".equals(accetHeader)) {
+//                return ResponseEntity.ok()
+//                        .body(studentList);
+//            }else{
+//                return  ResponseEntity
+//                        .status(HttpStatus.NOT_IMPLEMENTED)
+//                        .body("Parameter accetHeader is not supported");
+//            }
+//
+//        }catch (Exception e){
+//            return ResponseEntity
+//                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                    .body("Error Server");
+//        }
+//    }
 }
